0f930628c8ae26e1d99f82677b1c10a11cba288e
luunavasv <43542934+luunavasv@users.noreply.github.com>
2020-06-03 00:34:49 -0600

    Modal fixes

--- views/dashboard/suscription-plans.php
+++ views/dashboard/suscription-plans.php
@@ -13,7 +13,7 @@ Page::headerTemplate('Gestión de planes de suscripción', null);
     </div>
 </div>
 <div class="row">
-    <div class="col-md-10">
+    <div class="col-md-12">
         <table id="myTable" class="table table-responsive-sm table-hover">
             <thead>
                 <tr>
@@ -36,37 +36,37 @@ Page::headerTemplate('Gestión de planes de suscripción', null);
     </div>
 </div>
 <div class="modal fade" id="save-modal" tabindex="-1" role="dialog" aria-labelledby="save-modal" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-    <div class="modal-header">
-        <h5 class="modal-title" id="modal-title"></h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
-    <form method="post" id="save-form" enctype="">
-        <div class="modal-body p-md-4">
-            <!-- Campo oculto para asignar el id del registro al momento de modificar -->
-            <input class="d-none" type="text" id="idplansuscripcion" name="idplansuscripcion"/>
-            <p>Ingresa la cantidad de pares de calcetines y el precio de envio</p>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="cantidadpares">Cantidad de pares</label>
-                    <input type="text" class="form-control" placeholder="x pares" id="cantidadpares" name="cantidadpares">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="precio">Precio de envio</label>
-                    <input type="text" class="form-control" placeholder="Precio($)" id="precio" name="precio">
-                </div>
-            </div>            
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="modal-title"></h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-            <button type="submit" class="btn btn-purple">Guardar</button>
+        <form method="post" id="save-form" enctype="">
+            <div class="modal-body p-md-4">
+                <!-- Campo oculto para asignar el id del registro al momento de modificar -->
+                <input class="d-none" type="text" id="idplansuscripcion" name="idplansuscripcion"/>
+                <p>Ingresa la cantidad de pares de calcetines y el precio de envio</p>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="cantidadpares">Cantidad de pares</label>
+                        <input type="text" class="form-control" placeholder="x pares" id="cantidadpares" name="cantidadpares">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="precio">Precio de envio</label>
+                        <input type="text" class="form-control" placeholder="Precio($)" id="precio" name="precio">
+                    </div>
+                </div>            
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                <button type="submit" class="btn btn-purple">Guardar</button>
+            </div>
+        </form>   
         </div>
-    </form>   
     </div>
-</div>
 </div>
 <?php
 Page::footerTemplate( 'planSuscripcion.js' );
