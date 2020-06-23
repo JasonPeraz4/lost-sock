d52f9d46ce2843adb9f616f58822b2e88f9b38a2
luunavasv <43542934+luunavasv@users.noreply.github.com>
2020-06-22 19:01:53 -0600

    Pubic site added

---
+++ views/commerce/account.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../../core/helpers/template-commerce.php');
+Page::headerTemplate('Lost Sock');
+?>
+    <div class="container-fluid cover-general">
+      <div class="row">
+
+      </div>
+    </div>
+
+<?php
+Page::footerTemplate();
+?>
